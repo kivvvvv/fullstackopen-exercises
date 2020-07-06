c371342cc3868443fc26faab58962274f89d8deb
Sirapop Iam-o-pas <sirapop@mysocialmotion.org>
2020-07-06 09:21:39 +0700

    add part3::3.11: phonebook backend

--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -6,8 +6,8 @@ export default function Persons ({persons, searchKeyword, onDeletePersonByIdClic
 			{persons
 				.filter(person => person.name.toLowerCase().includes(searchKeyword))
 				.map(person => (
-					<div>
-						<span key={person.name}>{person.name} {person.number}</span>
+					<div key={person.id}>
+						<span>{person.name} {person.number}</span>
 						<button onClick={onDeletePersonByIdClick(person.id, person.name)}>delete</button>
 					</div>
 				))
